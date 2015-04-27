bceaf7e2a1c3dbd11d802b88239801ef11f89640
Krishna Patel <softeng.krishna@gmail.com>
2015-04-27 20:06:39 +0530

    Initial commitcsc

---
+++ app/src/main/java/android/krishna/com/quizdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package android.krishna.com.quizdemo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnStart = (Button)findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+
+
+
+}
